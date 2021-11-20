cdd4d24cb37e7650ef9b310ddc5d224707605f2a
Mohammed Boukhal <mboukhal@e1r8p1.1337.ma>
2021-11-20 08:23:15 +0100

    libft

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mboukhal <mboukhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 13:50:18 by mboukhal          #+#    #+#             */
-/*   Updated: 2021/11/15 13:56:27 by mboukhal         ###   ########.fr       */
+/*   Updated: 2021/11/15 17:27:30 by mboukhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,19 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		t = t->next;
 	t->next = new;
 }
+
+
+// int main ()
+// {
+// 	t_list* s = NULL;
+
+// 	ft_lstadd_back(&s, ft_lstnew((void*) 1));
+// 	ft_lstadd_back(&s, ft_lstnew((void*) 2));
+// 	if (s->content == (void*)1)
+// 		printf("1");
+// 	if (s->next->content == (void*)2)
+// 		printf("2");
+// 	if (s->next == NULL)
+// 		printf("0");
+
+// }
--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: mboukhal <mboukhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 10:39:03 by mboukhal          #+#    #+#             */
-/*   Updated: 2021/11/13 11:09:17 by mboukhal         ###   ########.fr       */
+/*   Updated: 2021/11/15 17:27:43 by mboukhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,25 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
 	*lst = new;
+}
+
+
+
+// int main(){
+
+// 	t_list * r = NULL ;
+
+// 	ft_lstadd_front(&r, ft_lstnew((void*)1));
+	
+// 	ft_lstadd_front(&r, ft_lstnew((void*)2));
+
+
+// 	if (r->content == (void*)2)
+// 		printf("2\n");
+// 	if (r->next->content == (void*)1)
+// 		printf("1\n");
+// 	if (!r->next->next)
+// 		printf("0\n");
+
+
 }
